7229556ce4390bb9c16ee05adf0237a8dc8903a3
LordRaleigh7926 <ankushroy7926@gmail.com>
2024-03-13 11:40:06 +0530

    Api keys removal

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,8 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import firebaseConfig from './firebase-api-keys';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAofpu8gJqB9-pBgyiSrpAjYYZ9A9RzY6g",
-    authDomain: "disney-in-react-18330.firebaseapp.com",
-    projectId: "disney-in-react-18330",
-    storageBucket: "disney-in-react-18330.appspot.com",
-    messagingSenderId: "357926808184",
-    appId: "1:357926808184:web:0395d207a47ffff1ac9b8d"
-};
 
 
 const firebaseApp = initializeApp(firebaseConfig);
